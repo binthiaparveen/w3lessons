282dd7486e26cb52a31ee1230f14a0f49df0d47a
binthiaparveen <133879182+binthiaparveen@users.noreply.github.com>
2023-12-12 09:10:23 +0600

    Add files via upload

---
+++ app/src/main/java/com/binthia/w3schools/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.binthia.w3schools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
